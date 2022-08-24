2a483219abfbe37d864e6eb3cdc4b88b6ed5da31
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-24 12:17:29 -0300

    Create armenia.js

---
+++ variables/Armenia flag/armenia.js
@@ -0,0 +1,18 @@
+//drawBox(red) on the app draws a red box
+
+drawBox(red);
+drawBox(red);
+drawBox(red);
+
+//newLine() on the app breaks onto a new line
+newLine();
+//drawBox(blue) on the app draws a red blue
+drawBox(blue);
+drawBox(blue);
+drawBox(blue);
+
+newLine();
+//drawBox(orange) on the app draws a orange box
+drawBox(orange);
+drawBox(orange);
+drawBox(orange);
