1c211e14eddecfee8605bda27ab1b17f14dfb802
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-24 12:18:56 -0300

    Create estonia.js

---
+++ variables/Estonian flag/estonia.js
@@ -0,0 +1,17 @@
+// drawBox(blue) on the app draws a blue box
+drawBox(blue);
+drawBox(blue);
+drawBox(blue);
+
+// newLine() on the apps breaks the line
+newLine();
+// drawBox(black) on the app draws a black box
+drawBox(black);
+drawBox(black);
+drawBox(black);
+
+newLine();
+// drawBox(white) on the app draws a white box
+drawBox(white);
+drawBox(white);
+drawBox(white);
