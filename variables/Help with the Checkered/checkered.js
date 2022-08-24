2a94048f1dfded857e8ff162acd4d92385e79f9b
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-24 12:20:52 -0300

    Create checkered.js

---
+++ variables/Help with the Checkered/checkered.js
@@ -0,0 +1,10 @@
+//A função drawBoxes() leva uma string como um argumento e é criada uma caixa para cada letra da string.Por exemplo, drawBoxes('brb') desenhará uma caixa azul, vermelha e outra azul.
+
+//draw a box 'k' draws a black box and 'w' draws a white box
+drawBoxes('kwk');
+
+//newLine() breaks into a new line
+newLine();
+drawBoxes('wkw');
+newLine();
+drawBoxes('kwk');
