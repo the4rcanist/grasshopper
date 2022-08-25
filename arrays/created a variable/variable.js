d572c47b0b99a6707e9c1dfb11e867fa42f49466
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 14:03:02 -0300

    Create variable.js

---
+++ arrays/created a variable/variable.js
@@ -0,0 +1,13 @@
+//Variáveis são muito comuns em programação, elas permitem que você faça referência à mesma informação várias vezes. Em Javascript, as variáveis podem ser definidas usando var e dando à variável um nome exclusivo. Por exemplo, var myVariable = 'sometext'. A variável pode posteriormente receber um novo valor. Por exemplo, myVariable = 'changed some text' Ao reatribuir uma variável, var não é necessário
+
+//Ex:
+
+var myExample;
+myExample = 'I created a variable';
+print(myExample);
+
+myExample = 'I changed the value';
+print(myExample);
+
+//A variável MyExample é declarada, na linha seguinte ela recebe um valor e então é impressa. O Valor da variável é então atualizado e impresso novamente com uma mensagem diferente.
+
