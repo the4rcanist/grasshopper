0ef6622ec6e2dee311d46ced37c0fac88ad50f39
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 14:04:18 -0300

    Create drink.js

---
+++ arrays/created a variable/drink.js
@@ -0,0 +1,11 @@
+//Qual será o resultado desse código?
+
+var drink = 'juice';
+print(drink);
+
+drink = 'water'
+print(drink);
+
+//R: juice; water;
+
+//A variável drink é atualizada com um novo valor antes de drink ser impresso usando a função print()
