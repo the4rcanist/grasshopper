c848f3c691abe3905ba3d7328a10bb9fda401006
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 13:55:56 -0300

    Create flag.js

---
+++ arrays/From Poland to San Marino/flag.js
@@ -0,0 +1,17 @@
+//Use uma variável para armazenar dados e, em seguida, alterará o valor dessa variável.
+//Por exemplo var insect = 'Grasshopper' salva a string 'Grasshopper' na variável insect
+
+//##Transforme a bandeira da Polônia na Bandeira simplificada de San Marino, alterando o valor da variável stripeColor
+//Mude red para blue
+
+var stripeColor = white;
+
+drawBox(stripeColor);
+drawBox(stripeColor);
+drawBox(stripeColor);
+newLine();
+
+stripeColor = blue;
+drawBox(stripeColor);
+drawBox(stripeColor);
+drawBox(stripeColor);
