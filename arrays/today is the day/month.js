25a4ebc3c7d8b843b4e6cdc81a309d8a05109af4
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 13:59:03 -0300

    Create month.js

---
+++ arrays/today is the day/month.js
@@ -0,0 +1,10 @@
+//Você declarará uma nova variável
+//Por exemplo, var month = 'August' cria uma variável chamada month e atribui o valor 'August' a ela
+//Declare uma nova variável para lembrar que day é esse
+//Crie uma variável chamada day, atribua a ela a string 'Friday', imprima a variável usando print(day)
+
+var month = 'August';
+print(month);
+
+var day = 'Friday';
+print(day);
