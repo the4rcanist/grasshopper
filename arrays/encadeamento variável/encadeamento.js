08d0ccefbade910ab4ff679b0e2c6449f2629455
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 14:09:25 -0300

    Create encadeamento.js

---
+++ arrays/encadeamento variável/encadeamento.js
@@ -0,0 +1,15 @@
+//Às vezes, as variáveis podem ser atribuídas a outras variáveis.
+
+y = 17;
+x = y;
+print (x);
+
+//No exemplo acima, x recebe o valor 17 porque y armazena o valor 17. Isso significa que o número 17 será impresso.
+//Qual será o resultado do código à seguir?
+
+var y = 5;
+var x = y;
+print(x);
+
+//R: 5
+//A declaração da variável x atribui o valor salvo a y. Neste exemplo, o valor de y é 5, a função print(x) imprimirá o número 5
