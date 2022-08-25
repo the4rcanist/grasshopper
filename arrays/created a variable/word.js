0311b8178f70f3e3cfa38fc42f917a9eb2a1566d
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 14:06:11 -0300

    Create word.js

---
+++ arrays/created a variable/word.js
@@ -0,0 +1,8 @@
+//Qual será o resultado do código a seguir?
+
+var word = 'alphabet';
+var alphabet = 'word';
+print(alphabet);
+
+//R: alphabet
+//A variável está armazenando o valor 'word'. Quando a função print() é executada com o argumento alphabet, é impresso word.
